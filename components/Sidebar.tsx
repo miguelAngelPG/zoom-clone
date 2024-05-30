4bbcc0400488cd2df15497f92ec401a923fce234
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-05-29 19:22:03 -0600

    add dialog component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 
 export const Sidebar = () => {
     const pathname = usePathname()
-    console.log(pathname)
+
     return (
         <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
             <div className='flex flex-1 flex-col gap-6'>
