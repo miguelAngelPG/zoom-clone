0e2f5801b029ae8e8fac9420f8af4f5eb3017e6d
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-05-31 15:32:37 -0600

    use provider and toast

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,12 @@
+import { StreamVideoProvider } from '@/providers/StreamClientProvider'
 import React, { ReactNode } from 'react'
 
 const layout = ({children}: {children: ReactNode}) => {
     return (
         <main>
-            {children}
+            <StreamVideoProvider>
+                {children}
+            </StreamVideoProvider>
         </main>
     )
 }
